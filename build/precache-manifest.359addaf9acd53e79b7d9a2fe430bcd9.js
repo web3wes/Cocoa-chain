484428e0240706faecb978f4eae509d9c2d9053f
wes <wes@wess-MacBook-Pro.local>
2022-02-08 22:26:58 -0500

    Cocoa-sign item signing test

---
+++ build/precache-manifest.359addaf9acd53e79b7d9a2fe430bcd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3c96f11798c44e5b9bb",
+    "url": "/static/css/main.186339e3.chunk.css"
+  },
+  {
+    "revision": "f3c96f11798c44e5b9bb",
+    "url": "/static/js/main.f3c96f11.chunk.js"
+  },
+  {
+    "revision": "33f885eaef00d927aae2",
+    "url": "/static/css/1.681dc4b4.chunk.css"
+  },
+  {
+    "revision": "33f885eaef00d927aae2",
+    "url": "/static/js/1.33f885ea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79dcf9b857a6766f9d8b7a2367fd695e",
+    "url": "/index.html"
+  }
+];
